40e03624b663be25bba2dd0db40ad0948734e587
rishdus <risheek.dustin@gmail.com>
2022-08-06 14:04:58 +0530

    updating ./roboshop-shell

--- cart.sh
+++ cart.sh
@@ -1,41 +1,77 @@
 echo setting nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo installing nodejs
 yum install nodejs -y &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo adding application user
 useradd roboshop &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo downloading application content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
 
 cd /home/roboshop &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo cleaning old application content
 rm -rf cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo extracting application archive
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo installing nodejs dependencies
 npm install &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo configuring cart systemd service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi
 
 echo starting cart service
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e0m"
+else
+  echo -e "\e[31mFAILURE\e0m"
+fi 
