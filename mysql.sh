f32f090cf7002b788504f40bf724ecabccd0e48a
rishdus <risheek.dustin@gmail.com>
2022-08-08 16:08:31 +0530

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 COMPONENT=mysql
 source common.sh
 
-if [ -z "$MYSQL_PASSWORD"]; then
+if [ -z "$MYSQL_PASSWORD" ]; then
   echo -e "\e[33m env variable MYSQL_PASSWORD is missing \e[0m"
   exit 1
 fi
