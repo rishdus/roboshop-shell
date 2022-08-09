88c2c9e68dc9778c50747e5bb8b6bf881ee86374
rishdus <risheek.dustin@gmail.com>
2022-08-09 15:52:27 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -19,7 +19,7 @@ unzip -o /tmp/frontend.zip &>>${LOG} && mv frontend-main/static/* . && mv fronte
 StatusCheck
 
 echo update Nginx configuration
-sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 StatusCheck
 
 echo start Nginx Service
