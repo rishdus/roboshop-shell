19a6c135ccad2d2f1a672e283e3c030125fbbae2
rishdus <risheek.dustin@gmail.com>
2022-08-09 15:33:17 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -18,6 +18,10 @@ echo Extract downloaded content
 unzip -o /tmp/frontend.zip &>>${LOG} && mv frontend-main/static/* . && mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 StatusCheck
 
+echo update Nginx configuration
+sed -i -e 'catalogue/ s/localhost/catalogue-dev.roboshop.internal/' -e 'cart/ s/localhost/cart-dev.roboshop.internal/' -e 'user/ s/localhost/user-dev.roboshop.internal/' -e 'shipping/ s/localhost/shipping-dev.roboshop.internal/' -e 'payment/ s/localhost/payment-dev.roboshop.internal/'
+StatusCheck
+
 echo start Nginx Service
 systemctl restart nginx &>>${LOG} && systemctl enable nginx &>>${LOG}
 StatusCheck
